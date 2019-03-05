fa1d02c53f954be03c5b97003d302ae426928eea
bam forrest <cameron.forr@gmail.com>
2019-03-04 21:26:59 -0600

    added gif display function

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -23,17 +23,68 @@ var sportsPlayer = ["Michael Jordan", "Allen Iverson", "Odell Beckham", "Tracy M
 //On click events for buttons to display pictures
 $(document).on("click", "#add-player", function () {
 
+    
+
     //storing data name
-        var input = $("#player-input").val().trim();
+       sportsPlayer.push($("#player-input").val().trim());
     //New button creation
         var createSearchBtn = $("<button>");
     
-        createSearchBtn.text(input);
-        createSearchBtn.attr("players");
+        createSearchBtn.text(sportsPlayer[i]);
+        createSearchBtn.attr("class", "players").attr("data-name", sportsPlayer[i]);
         $(".buttons-view").append(createSearchBtn);
     
     });
 
+//function that Drops gifs onto page
+$(document).on("click", ".players", function () {
+    
+    
+// Grabbing and storing the data-animal property value from the button
+    var playaNames = $(this).attr("data-name");
+    console.log(playaNames);
+
+    // Constructing a queryURL using the animal name
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+      playaNames + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+        // After data comes back from the request
+        .then(function(response) {
+          console.log(queryURL);
+
+          console.log(response);
+
+          var results = response.data;
+
+          // Looping through each result item
+          for (var i = 0; i < results.length; i++) {
+
+            // Creating and storing a div tag
+            var playasDiv = $("<div>");
+
+            // Creating a paragraph tag with the result item's rating
+            var p = $("<p>").text("Rating: " + results[i].rating);
+
+            // Creating and storing an image tag
+            var playerImage = $("<img>");
+            // Setting the src attribute of the image to a property pulled off the result item
+            playerImage.attr("src", results[i].images.fixed_height.url);
+
+            // Appending the paragraph and image tag to the animalDiv
+            playasDiv.append(p);
+            playasDiv.append(playerImage);
+
+            // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+            $("#gif-dumps").prepend(playasDiv);
+          }
+        })
+
+})
 
 
 
